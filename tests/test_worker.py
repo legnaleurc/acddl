4279a7ce86847bb99309ef4ac09241feae927d1e
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-07-06 05:12:37 +0900

    add test case

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -43,17 +43,23 @@ class TestAsyncWorker(unittest.TestCase):
     @um.patch('threading.Condition', autospec=True)
     @um.patch('threading.Thread', autospec=True)
     def testStartTwice(self, FakeThread, FakeCondition, FakeIOLoop):
-        w = worker.AsyncWorker()
-        w._thread.is_alive.return_value = False
-        def thread_start_side_effect():
-            w._run()
-            w._thread.is_alive.return_value = True
-        w._thread.start.side_effect = thread_start_side_effect
+        w = self._createTemporaryWorker()
         w.start()
+
         the_loop = w._loop
         w.start()
         w._thread.start.assert_called_once_with()
         self.assertEqual(the_loop, w._loop)
+
+    @um.patch('tornado.ioloop.IOLoop', autospec=True)
+    @um.patch('threading.Condition', autospec=True)
+    @um.patch('threading.Thread', autospec=True)
+    def testStopTwice(self, FakeThread, FakeCondition, FakeIOLoop):
+        w = self._createTemporaryWorker()
+        w.start()
+        w.stop()
+
+        w.stop()
 
     def testDoWithSync(self):
         fn = self._createSyncMock()
@@ -105,6 +111,15 @@ class TestAsyncWorker(unittest.TestCase):
         async_call(functools.partial(tg.sleep, 0.001))
         fn.assert_called_once_with(1, k=7)
         fn.assert_awaited()
+
+    def _createTemporaryWorker(self):
+        w = worker.AsyncWorker()
+        w._thread.is_alive.return_value = False
+        def thread_start_side_effect():
+            w._run()
+            w._thread.is_alive.return_value = True
+        w._thread.start.side_effect = thread_start_side_effect
+        return w
 
     def _createSyncMock(self):
         return um.Mock(return_value=42)
