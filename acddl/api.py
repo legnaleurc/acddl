df5db04960b98983e9a690fe812f084e3d7bad69
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-07-11 15:23:48 +0800

    fix

--- acddl/api.py
+++ acddl/api.py
@@ -21,22 +21,22 @@ class NodesHandler(web.RequestHandler):
         controller = self.settings['controller']
         controller.update_cache_from(acd_paths)
 
-    def put(self, id_):
+    async def put(self, id_):
         if id_ is None:
             self.set_status(400)
             return
 
         controller = self.settings['controller']
-        controller.download(id_)
+        await controller.download(id_)
 
 
 class EqualityHandler(web.RequestHandler):
 
-    def get(self):
+    async def get(self):
         nodes = self.get_arguments('nodes[]')
 
         controller = self.settings['controller']
-        result = controller.compare(nodes)
+        result = await controller.compare(nodes)
         # iDontCare
         result = json.dumps(result)
         self.write(result)
