2d7949d735063d563ff6e255b5e34bba72483be6
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-07-11 11:17:35 +0800

    refactor test

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,134 +1,13 @@
 import functools
 import unittest as ut
 from unittest import mock as utm
-import datetime as dt
 import pathlib
-import hashlib
 
 from tornado import ioloop as ti, gen as tg
 from pyfakefs import fake_filesystem as ffs
 
 from acddl import controller as ctrl
 from . import util as u
-
-
-class PathMock(utm.Mock):
-
-    def __init__(self, fs=None, *pathsegments, **kwargs):
-        super(PathMock, self).__init__()
-
-        self._fs = fs
-        self._path = self._fs.JoinPaths(*pathsegments)
-
-    def iterdir(self):
-        fake_os = ffs.FakeOsModule(self._fs)
-        for child in fake_os.listdir(self._path):
-            yield PathMock(self._fs, self._path, child)
-
-    def stat(self):
-        fake_os = ffs.FakeOsModule(self._fs)
-        return fake_os.stat(self._path)
-
-    def mkdir(self, mode=0o777, parents=False, exist_ok=False):
-        fake_os = ffs.FakeOsModule(self._fs)
-        try:
-            fake_os.makedirs(self._path)
-        except OSError as e:
-            # iDontCare
-            pass
-        return True
-
-    def is_file(self):
-        fake_os = ffs.FakeOsModule(self._fs)
-        return fake_os.path.isfile(self._path)
-
-    def unlink(self):
-        fake_os = ffs.FakeOsModule(self._fs)
-        return fake_os.unlink(self._path)
-
-    def __truediv__(self, name):
-        return PathMock(self._fs, self._path, name)
-
-    def __str__(self):
-        return self._path
-
-
-class NodeMock(utm.Mock):
-
-    def __init__(self, fs, path, *args, **kwargs):
-        super(NodeMock, self).__init__()
-
-        self._fs = fs
-        self._path = path
-
-    @property
-    def name(self):
-        dirname, basename = self._fs.SplitPath(self._path)
-        return basename
-
-    @property
-    def modified(self):
-        f = self._fs.GetObject(self._path)
-        return dt.datetime.fromtimestamp(f.st_mtime)
-
-    @property
-    def is_available(self):
-        return True
-
-    @property
-    def is_folder(self):
-        fake_os = ffs.FakeOsModule(self._fs)
-        return fake_os.path.isdir(self._path)
-
-    @property
-    def size(self):
-        fake_os = ffs.FakeOsModule(self._fs)
-        return fake_os.path.getsize(self._path)
-
-    @property
-    def md5(self):
-        fake_open = ffs.FakeFileOpen(self._fs)
-        hasher = hashlib.md5()
-        with fake_open(self._path, 'rb') as fin:
-            while True:
-                chunk = fin.read(65536)
-                if not chunk:
-                    break
-                hasher.update(chunk)
-        return hasher.hexdigest()
-
-
-def create_fake_local_file_system():
-    fs = ffs.FakeFilesystem()
-    file_1 = fs.CreateFile('/local/file_1.txt', contents='file 1')
-    file_1.st_mtime = 1467800000
-    file_2 = fs.CreateFile('/local/folder_1/file_2.txt', contents='file 2')
-    file_2.st_mtime = 1467801000
-    folder_1 = fs.GetObject('/local/folder_1')
-    folder_1.st_mtime = 1467802000
-    return fs
-
-
-def create_fake_remote_file_system():
-    fs = ffs.FakeFilesystem()
-    file_3 = fs.CreateFile('/remote/file_3.txt', contents='file 3')
-    file_3.st_mtime = 1467803000
-    file_4 = fs.CreateFile('/remote/folder_2/file_4.txt', contents='file 4')
-    file_4.st_mtime = 1467804000
-    folder_2 = fs.GetObject('/remote/folder_2')
-    folder_2.st_mtime = 1467805000
-    return fs
-
-
-def metapathmock(fs, *args, **kwargs):
-    class ConcretePathMock(PathMock):
-
-        def __init__(self, *args, **kwargs):
-            super(ConcretePathMock, self).__init__(fs, *args, **kwargs)
-
-    return ConcretePathMock
-
-create_nodemock = functools.partial(lambda fs, *args, **kwargs: NodeMock(fs, *args, **kwargs), fs=create_fake_remote_file_system())
 
 
 class TestDownloadController(ut.TestCase):
@@ -152,9 +31,9 @@ class TestDownloadController(ut.TestCase):
 
     @utm.patch('acddl.worker.AsyncWorker', autospec=True)
     def testDownloadFrom(self, FakeAsyncWorker):
-        lfs = create_fake_local_file_system()
-        rfs = create_fake_remote_file_system()
-        with utm.patch('pathlib.Path', new_callable=functools.partial(metapathmock, lfs)) as FakePath:
+        lfs = u.create_fake_local_file_system()
+        rfs = u.create_fake_remote_file_system()
+        with utm.patch('pathlib.Path', new_callable=functools.partial(u.metapathmock, lfs)) as FakePath:
             context = utm.Mock()
             # mock acd_db
             context.db.sync = u.AsyncMock()
@@ -171,9 +50,9 @@ class TestDownloadController(ut.TestCase):
     @utm.patch('os.statvfs')
     @utm.patch('acddl.worker.AsyncWorker', autospec=True)
     def testDownload(self, FakeAsyncWorker, fake_statvfs, fake_utime):
-        lfs = create_fake_local_file_system()
-        rfs = create_fake_remote_file_system()
-        with utm.patch('pathlib.Path', new_callable=functools.partial(metapathmock, lfs)) as FakePath:
+        lfs = u.create_fake_local_file_system()
+        rfs = u.create_fake_remote_file_system()
+        with utm.patch('pathlib.Path', new_callable=functools.partial(u.metapathmock, lfs)) as FakePath:
             context = utm.Mock()
             # mock client
             context.client.download_node = fake_download_node
@@ -189,7 +68,7 @@ class TestDownloadController(ut.TestCase):
             vfs.f_bavail = 10 * 1024 ** 3
 
             dc = ctrl.DownloadController(context)
-            u.async_call(dc._download, NodeMock(rfs, '/remote/folder_2'), context.root, True)
+            u.async_call(dc._download, u.NodeMock(rfs, '/remote/folder_2'), context.root, True)
 
             l_fake_os = ffs.FakeOsModule(lfs)
             assert l_fake_os.path.isdir('/local/folder_2')
@@ -197,12 +76,12 @@ class TestDownloadController(ut.TestCase):
 
 
 async def fake_resolve_path(fs, remote_path):
-    return NodeMock(fs, remote_path)
+    return u.NodeMock(fs, remote_path)
 
 async def fake_get_children(fs, node):
     fake_os = ffs.FakeOsModule(fs)
     children = fake_os.listdir(node._path)
-    children = [NodeMock(fs, fs.JoinPaths(node._path, _)) for _ in children]
+    children = [u.NodeMock(fs, fs.JoinPaths(node._path, _)) for _ in children]
     return children
 
 async def fake_get_path(fs, node):
@@ -223,11 +102,4 @@ async def fake_download_node(node, local_path):
                 break
             fout.write(chunk)
 
-    hasher = hashlib.md5()
-    with l_fake_open(local_file, 'rb') as fin:
-        while True:
-            chunk = fin.read(65536)
-            if not chunk:
-                break
-            hasher.update(chunk)
-    return hasher.hexdigest()
+    return u.get_md5(l_fake_open, local_file)
--- tests/util.py
+++ tests/util.py
@@ -1,10 +1,13 @@
 import functools
-from unittest import mock as um
+from unittest import mock as utm
+import datetime as dt
+import hashlib
 
 from tornado import ioloop as ti, gen as tg
+from pyfakefs import fake_filesystem as ffs
 
 
-class AsyncMock(um.Mock):
+class AsyncMock(utm.Mock):
 
     def __init__(self, return_value=None):
         super(AsyncMock, self).__init__(return_value=self)
@@ -21,5 +24,126 @@ class AsyncMock(um.Mock):
         assert self._awaited
 
 
+class PathMock(utm.Mock):
+
+    def __init__(self, fs=None, *pathsegments, **kwargs):
+        super(PathMock, self).__init__()
+
+        self._fs = fs
+        self._path = self._fs.JoinPaths(*pathsegments)
+
+    def iterdir(self):
+        fake_os = ffs.FakeOsModule(self._fs)
+        for child in fake_os.listdir(self._path):
+            yield PathMock(self._fs, self._path, child)
+
+    def stat(self):
+        fake_os = ffs.FakeOsModule(self._fs)
+        return fake_os.stat(self._path)
+
+    def mkdir(self, mode=0o777, parents=False, exist_ok=False):
+        fake_os = ffs.FakeOsModule(self._fs)
+        try:
+            fake_os.makedirs(self._path)
+        except OSError as e:
+            # iDontCare
+            pass
+        return True
+
+    def is_file(self):
+        fake_os = ffs.FakeOsModule(self._fs)
+        return fake_os.path.isfile(self._path)
+
+    def unlink(self):
+        fake_os = ffs.FakeOsModule(self._fs)
+        return fake_os.unlink(self._path)
+
+    def __truediv__(self, name):
+        return PathMock(self._fs, self._path, name)
+
+    def __str__(self):
+        return self._path
+
+
+class NodeMock(utm.Mock):
+
+    def __init__(self, fs, path, *args, **kwargs):
+        super(NodeMock, self).__init__()
+
+        self._fs = fs
+        self._path = path
+
+    @property
+    def name(self):
+        dirname, basename = self._fs.SplitPath(self._path)
+        return basename
+
+    @property
+    def modified(self):
+        f = self._fs.GetObject(self._path)
+        return dt.datetime.fromtimestamp(f.st_mtime)
+
+    @property
+    def is_available(self):
+        return True
+
+    @property
+    def is_folder(self):
+        fake_os = ffs.FakeOsModule(self._fs)
+        return fake_os.path.isdir(self._path)
+
+    @property
+    def size(self):
+        fake_os = ffs.FakeOsModule(self._fs)
+        return fake_os.path.getsize(self._path)
+
+    @property
+    def md5(self):
+        fake_open = ffs.FakeFileOpen(self._fs)
+        return get_md5(fake_open, self._path)
+
+
 def async_call(fn, *args, **kwargs):
     return ti.IOLoop.instance().run_sync(functools.partial(fn, *args, **kwargs))
+
+
+def create_fake_local_file_system():
+    fs = ffs.FakeFilesystem()
+    file_1 = fs.CreateFile('/local/file_1.txt', contents='file 1')
+    file_1.st_mtime = 1467800000
+    file_2 = fs.CreateFile('/local/folder_1/file_2.txt', contents='file 2')
+    file_2.st_mtime = 1467801000
+    folder_1 = fs.GetObject('/local/folder_1')
+    folder_1.st_mtime = 1467802000
+    return fs
+
+
+def create_fake_remote_file_system():
+    fs = ffs.FakeFilesystem()
+    file_3 = fs.CreateFile('/remote/file_3.txt', contents='file 3')
+    file_3.st_mtime = 1467803000
+    file_4 = fs.CreateFile('/remote/folder_2/file_4.txt', contents='file 4')
+    file_4.st_mtime = 1467804000
+    folder_2 = fs.GetObject('/remote/folder_2')
+    folder_2.st_mtime = 1467805000
+    return fs
+
+
+def metapathmock(fs, *args, **kwargs):
+    class ConcretePathMock(PathMock):
+
+        def __init__(self, *args, **kwargs):
+            super(ConcretePathMock, self).__init__(fs, *args, **kwargs)
+
+    return ConcretePathMock
+
+
+def get_md5(open_, path):
+    hasher = hashlib.md5()
+    with open_(path, 'rb') as fin:
+        while True:
+            chunk = fin.read(65536)
+            if not chunk:
+                break
+            hasher.update(chunk)
+    return hasher.hexdigest()
