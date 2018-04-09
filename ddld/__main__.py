3e97e24a39a48259fd74e2c20aaa5ffeb816a8c9
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-04-09 22:45:45 +0800

    rewrite main

--- ddld/__main__.py
+++ ddld/__main__.py
@@ -1,23 +1,38 @@
 import argparse
+import asyncio
 import logging
 import os.path as op
 import signal
 import sys
 
-from tornado import ioloop as ti, web as tw, httpserver as ths
+from aiohttp import web as aw
+import aiohttp_jinja2 as aj
+import jinja2
 from wcpan.logger import setup as setup_logger, INFO
 
 from . import util, api, view
 from .controller import RootController
 
 
+class DaemonHelper(object):
+
+    def __init__(self):
+        self._finished = asyncio.Event()
+
+    def on_stop(self):
+        self._finished.set()
+
+    async def start(self):
+        await self._finished.wait()
+
+
 def main(args=None):
     if args is None:
         args = sys.argv
 
-    main_loop = ti.IOLoop.instance()
-    main_loop.add_callback(amain, args)
-    main_loop.start()
+    main_loop = asyncio.get_event_loop()
+    main_loop.create_task(amain(args))
+    main_loop.run_forever()
     main_loop.close()
     return 0
 
@@ -26,44 +41,45 @@ async def amain(args):
     args = parse_args(args[1:])
 
     loggers = setup_logger((
-        'tornado.access',
-        'tornado.application',
-        'tornado.general',
-        'requests.packages.urllib3.connectionpool',
+        'aiohttp',
         'wcpan.drive.google',
         'wcpan.worker',
         'ddld',
-    ), '/tmp/ddld.log')
+    ))
     logs = util.LogQueue(logging.DEBUG)
     for logger in loggers:
         logger.addHandler(logs)
 
-    main_loop = ti.IOLoop.instance()
-    controller = RootController(args.root)
+    main_loop = asyncio.get_event_loop()
 
-    await controller.initialize()
+    application = aw.Application()
 
-    async def close_root():
-        await controller.close()
-        main_loop.stop()
-    def close_signal(signum, frame):
-        main_loop.add_callback_from_signal(close_root)
-    signal.signal(signal.SIGINT, close_signal)
+    setup_static_and_view(application)
+    setup_api_path(application)
 
-    static_path = op.join(op.dirname(__file__), 'static')
-    application = tw.Application([
-        (r'/api/v1/nodes', api.NodesHandler),
-        (r'/api/v1/nodes/([a-zA-Z0-9\-_]+)', api.NodesHandler),
-        (r'/api/v1/cache', api.CacheHandler),
-        (r'/api/v1/cache/([a-zA-Z0-9\-_]+)', api.CacheHandler),
-        (r'/', view.IndexHandler),
-        (r'/api/v1/log', api.LogHandler),
-        (r'/api/v1/socket', api.LogSocketHandler),
-    ], static_path=static_path, controller=controller, logs=logs)
-    server = ths.HTTPServer(application)
-    server.listen(args.listen)
+    lsh = api.LogSocketHandler(application)
+    application.router.add_view(r'/api/v1/socket', lsh.handle)
 
-    INFO('ddld') << 'ready'
+    daemon = DaemonHelper()
+    main_loop.add_signal_handler(signal.SIGINT, daemon.on_stop)
+
+    async with RootController(args.root) as controller:
+        application['controller'] = controller
+        application['logs'] = logs
+
+        runner = aw.AppRunner(application)
+        await runner.setup()
+        site = aw.TCPSite(runner, port=args.listen)
+        await site.start()
+
+        INFO('ddld') << 'ready'
+
+        await daemon.start()
+
+        await lsh.close()
+        await runner.cleanup()
+
+    main_loop.stop()
 
     return 0
 
@@ -77,6 +93,27 @@ def parse_args(args):
     args = parser.parse_args(args)
 
     return args
+
+
+def setup_static_and_view(app):
+    root = op.dirname(__file__)
+    static_path = op.join(root, 'static')
+    template_path = op.join(root, 'templates')
+
+    app.router.add_static('/static/', path=static_path, name='static')
+    app['static_root_url'] = '/static'
+
+    aj.setup(app, loader=jinja2.FileSystemLoader(template_path))
+
+    app.router.add_view(r'/', view.IndexHandler)
+
+
+def setup_api_path(app):
+    application.router.add_view(r'/api/v1/nodes', api.NodesHandler)
+    application.router.add_view(r'/api/v1/nodes/{id:[a-zA-Z0-9\-_]+}', api.NodesHandler)
+    application.router.add_view(r'/api/v1/cache', api.CacheHandler)
+    application.router.add_view(r'/api/v1/cache/{id:[a-zA-Z0-9\-_]+}', api.CacheHandler)
+    application.router.add_view(r'/api/v1/log', api.LogHandler)
 
 
 exit_code = main()
